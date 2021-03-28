50b3798e13e3faacd1acd20cb90070c720e4cd5f
keih <keih.te.pub@gmail.com>
2021-03-28 09:11:24 +0000

    ex.fav dev model routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,20 @@ class User < ApplicationRecord
                       uniqueness: { case_sensitive: false }
     has_secure_password
     
+    #follow
     has_many :microposts
     has_many :relationships
     has_many :followings, through: :relationships, source: :follow
     has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
     has_many :followers, through: :reverses_of_relationship, source: :user
     
+    #お気に入り
+    has_many :favorites
+    has_many :microposts, through: :favorites, source: :micropost
+    has_many :reverses_of_relationship, class_name: 'favorite', foreign_key: 'micropost_id'
+    has_many :users, through: :reverses_of_relationship, source: :user
+    
+    #follow
     def follow(other_user)
         unless self == other_user
             self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -29,6 +37,21 @@ class User < ApplicationRecord
     
     def feed_microposts
         Micropost.where(user_id: self.following_ids + [self.id])
+    end
+    
+    #お気に入り
+    
+    def favor(micropost)
+        self.favorites.find_or_create_by(micropost_id: micropost.id)
+    end
+    
+    # def unfavor(other_micropost)
+    #     favorite= self.favorites.find_by(micropost_id: other_micropost.id)
+    #     favorite.destroy if favorite
+    # end
+    
+    def favoring?(micropost)
+        self.favorites.exists?(micropost_id: micropost.id)
     end
     
 end
